ab429a8489098d56a6aff5ad34f135f2e4f43384
Paulami-B <paulamiibanerjee@gmail.com>
2025-04-04 10:47:14 +0530

    Left Panel added

---
+++ src/components/home/ThemeSwitch.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+import React from 'react'
+import { Button } from '../ui/button'
+import { MoonIcon, SunIcon } from 'lucide-react'
+import { useTheme } from 'next-themes'
+
+export default function ThemeSwitch() {
+    const { setTheme } = useTheme();
+    return (
+        <DropdownMenu>
+			<DropdownMenuTrigger asChild className='bg-transparent relative'>
+				<Button variant='outline' size='icon'>
+					<SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+					<MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+					<span className='sr-only'>Toggle theme</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align='end' className='bg-gray-primary dark:bg-dark-gray-primary'>
+				<DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+    )
+}
